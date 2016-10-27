6f6f29266558d8e58aebe3c081d4d0eaa66a2cdc
BORIS <githubcream.soundwave2011@recursor.net>
2016-10-26 22:51:35 -0400

    Create actorattributes.js

---
+++ actorattributes.js
@@ -0,0 +1,10 @@
+var maxhp = $gameActors.actor(1)._paramPlus[0];
+var maxmp = $gameActors.actor(1)._paramPlus[1];
+var atk = $gameActors.actor(1)._paramPlus[2];
+var def = $gameActors.actor(1)._paramPlus[3];
+var magicdmg = $gameActors.actor(1)._paramPlus[4];
+var magicdef = $gameActors.actor(1)._paramPlus[5];
+var agi = $gameActors.actor(1)._paramPlus[6];
+var luk = $gameActors.actor(1)._paramPlus[7];
+
+//add 1 for each attribute
